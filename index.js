3298d4ce8d2efacc8d070b48ae0d868f9dbbf41e
Adam Kalt <adam.kalt.1@gmail.com>
2018-01-24 00:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if () {
+  if (logWhisper) {
     return "I can\'t hear you!";
-} else if () {
+} else if (shout) {
     return "YES INDEED";
 } else {
     return "I love you too";
